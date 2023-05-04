13908d76b23fa4ad88aeff925cdbaa49db632283
Tori Lew <torirlew@gmail.com>
2023-05-04 16:08:39 -0700

    Tap and long press gestures

--- Chapter11/GettingStarted.swift
+++ Chapter11/GettingStarted.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct GettingStartedExerciseView: View {
-
+    @State var rectColor = Color.gray
+    @State var textColor = Color.black
     
     var body: some View {
         VStack {
             Text("Getting started exercise goes here")
                 .font(.largeTitle)
+            ZStack{
+                Rectangle()
+                    .frame(width: 300, height: 150)
+                    .foregroundColor(rectColor)
+
+                Text("Tap to change color, hold to go \nback to grey")
+                    .foregroundColor(textColor)
+
+                    
+            }.onTapGesture {
+                if rectColor == .black {
+                    rectColor = .cyan
+                    textColor = .black
+                } else {
+                    rectColor = .black
+                    textColor = .cyan
+                }
+            }
+            .onLongPressGesture(minimumDuration: 2, maximumDistance: 2, pressing: {stillPressed in
+                //  Code to run while the long press occurs
+            }) {
+                //  Code to run after detecting the long press gesture
+                rectColor = Color.gray
+            }
         }
         .padding()
     }
