685796e4c4c5448278275f0395ea73cba07789f6
Nick Eddington <neddington@cascadia.edu>
2023-05-04 18:35:45 -0700

    Testing

--- Chapter11/GettingStarted.swift
+++ Chapter11/GettingStarted.swift
@@ -8,14 +8,82 @@
 import SwiftUI
 
 struct GettingStartedExerciseView: View {
-
+    
+    @State private var rectangleColor = Color.blue
+    @State private var rotationAngle: Angle = .degrees(0)
+    @State private var scale: CGFloat = 1.0
+    @State private var dragOffset: CGSize = .zero
+    @State private var resetColor = false
     
     var body: some View {
-        VStack {
-            Text("Getting started exercise goes here")
-                .font(.largeTitle)
-        }
-        .padding()
+        let dragGesture = DragGesture()
+            .onChanged { gesture in
+                dragOffset = gesture.translation
+                resetColor = true
+            }
+            .onEnded { _ in
+                dragOffset = .zero
+                resetColor = false
+            }
+        
+        let longPressGesture = LongPressGesture(minimumDuration: 2.0)
+            .onEnded { _ in
+                rectangleColor = .blue
+            }
+        
+        let rotationGesture = RotationGesture()
+            .onChanged { gesture in
+                rotationAngle = gesture.degreesToRadians
+                resetColor = true
+            }
+            .onEnded { _ in
+                rotationAngle = .degrees(0)
+                resetColor = false
+            }
+        
+        let magnificationGesture = MagnificationGesture()
+            .onChanged { gesture in
+                scale = gesture.magnitude
+                resetColor = true
+            }
+            .onEnded { _ in
+                scale = 1.0
+                resetColor = false
+            }
+        
+        let tapGesture = TapGesture()
+            .onEnded { _ in
+                rectangleColor = Color(red: Double.random(in: 0...1),
+                                       green: Double.random(in: 0...1),
+                                       blue: Double.random(in: 0...1))
+            }
+        
+        let gesture = longPressGesture
+            .sequenced(before: dragGesture)
+            .exclusively(before: rotationGesture)
+            .exclusively(before: magnificationGesture)
+            .simultaneously(with: tapGesture)
+        
+        let rectangle = Rectangle()
+            .fill(rectangleColor)
+            .overlay(Text("Hello, world!")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.black.opacity(0.7)))
+            .frame(width: 200, height: 100)
+            .rotationEffect(rotationAngle)
+            .scaleEffect(scale)
+            .offset(x: dragOffset.width, y: dragOffset.height)
+            .gesture(gesture)
+        
+        return rectangle
+            .padding()
+    }
+}
+
+private extension Angle {
+    var degreesToRadians: Angle {
+        return self * .pi / 180
     }
 }
 
--- Chapter11/Map.swift
+++ Chapter11/Map.swift
@@ -1,21 +1,96 @@
-//
-//  ContentView.swift
-//  Chapter11
-//
-//  Created by Mike Panitz on 4/18/23.
-//
-
 import SwiftUI
+import MapKit
 
 struct MapExerciseView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 47.6062, longitude: -122.3321),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    @State private var annotation: MKPointAnnotation?
+    @State private var mapView: MKMapView?
 
-    
     var body: some View {
-        VStack {
-            Text("Map exercise goes here")
-                .font(.largeTitle)
+        ZStack(alignment: .topTrailing) {
+            Map(coordinateRegion: $region, annotation: annotation, showsUserLocation: true)
+                .edgesIgnoringSafeArea(.all)
+                .gesture(
+                    LongPressGesture(minimumDuration: 1.0)
+                        .onEnded { gesture in
+                            guard let mapView = mapView else { return }
+                            let location = gesture.location
+                            let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+                            let annotation = MKPointAnnotation()
+                            annotation.coordinate = coordinate
+                            self.annotation = annotation
+                        }
+                )
+
+
+
+            Compass()
+                .frame(width: 50, height: 50)
+                .padding(.trailing)
+                .padding(.top)
         }
-        .padding()
+    }
+}
+
+struct Map: UIViewRepresentable {
+    @Binding var coordinateRegion: MKCoordinateRegion
+    var annotation: MKPointAnnotation?
+    var showsUserLocation: Bool
+
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        mapView.showsUserLocation = showsUserLocation
+        return mapView
+    }
+
+    func updateUIView(_ mapView: MKMapView, context: Context) {
+        mapView.setRegion(coordinateRegion, animated: true)
+
+        if let annotation = annotation {
+            mapView.removeAnnotations(mapView.annotations)
+            mapView.addAnnotation(annotation)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: Map
+
+        init(_ parent: Map) {
+            self.parent = parent
+        }
+
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let identifier = "Annotation"
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            if annotationView == nil {
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = true
+            } else {
+                annotationView?.annotation = annotation
+            }
+            return annotationView
+        }
+    }
+}
+
+
+struct Compass: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKCompassButton {
+        let compass = MKCompassButton(mapView: MKMapView())
+        compass.compassVisibility = .adaptive
+        return compass
+    }
+
+    func updateUIView(_ uiView: MKCompassButton, context: Context) {
+        // No need to update anything here
     }
 }
 
